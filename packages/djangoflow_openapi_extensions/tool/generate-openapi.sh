ddb2101ef384139532785136a866adde1313e6dc
Shatanik Mahanty <shatanikmahanty@gmail.com>
2023-11-29 10:07:59 +0530

    Update generate-openapi.sh
    Added windows option

--- packages/djangoflow_openapi_extensions/tool/generate-openapi.sh
+++ packages/djangoflow_openapi_extensions/tool/generate-openapi.sh
@@ -9,7 +9,7 @@ API_VERSION=${2:-"v1"}
 
 echo "Generating OpenAPI client using host: ${HOST_NAME}"
 [ -d "${TARGET_DIR}" ] || mkdir -p "${TARGET_DIR}"
-${OPENAPI_GENERATOR:-openapi-generator} generate -g dart-dio -p browserClient=false -p nullableFields=true \
+${OPENAPI_GENERATOR:-openapi-generator-cli} generate -g dart-dio -p browserClient=false -p nullableFields=true \
   -p serializationLibrary=json_serializable -p pubName=djangoflow_openapi \
   -p pubLibrary=djangoflow_openapi \
   -i "${HOST_NAME}/api/${API_VERSION}/schema" -o "${TARGET_DIR}" \
